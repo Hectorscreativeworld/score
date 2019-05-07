6be0976858864b421f071d0df937e60cd0d64ea2
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-05-06 21:24:00 -0400

    game-1

--- public/main.js
+++ public/main.js
@@ -1,7 +1,52 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let teamOneScore = 0
+let teamTwoScore = 0
+
+
+
+// add score //
+const addScoreTeamOne = () => {
+  if (teamOneScore < 21) {
+    teamOneScore += 1
+    document.querySelector('.team1Score').textContent = teamOneScore
+  }
+}
+const addScoreTeamTwo = () => {
+  if (teamTwoScore < 21) {
+    teamTwoScore += 1
+    document.querySelector('.team2Score').textContent = teamTwoScore
   }
 }
 
+// subtract score //
+const subtractScoreTeamOne = () => {
+  if (teamOneScore > 0) {
+    teamOneScore += -1
+    document.querySelector('.team1Score').textContent = teamOneScore
+  }
+}
+const subtractScoreTeamTwo = () => {
+  if (teamTwoScore > 0) {
+    teamTwoScore += -1
+    document.querySelector('.team2Score').textContent = teamTwoScore
+  }
+}
+
+
+
+// add Event Listener //
+
+const main = () => {
+  document.querySelector('.team-1-add-1-button')
+    .addEventListener('click', addScoreTeamOne)
+  document.querySelector('.team-1-subtract-1-button').addEventListener('click', subtractScoreTeamOne)
+  document.querySelector('.team-2-add-1-button').addEventListener('click', addScoreTeamTwo)
+  document.querySelector('.team-2-subtract-1-button').addEventListener('click', subtractScoreTeamTwo)
+  document.querySelector('.update-team-one-input').addEventListener('click'
+    updateTeamOne)
+
+}
+
+
 document.addEventListener('DOMContentLoaded', main)
+
+// type vaules inside box and click update button to return new team name
